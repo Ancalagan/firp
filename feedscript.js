16e3cc6f5b9519d306190fc6234e9a90f3c863f4
Ancalagan <3ancalagon@gmail.com>
2024-11-26 15:49:07 +0200

    Add files via upload

---
+++ feedscript.js
@@ -0,0 +1,8 @@
+const event = document.getElementById("btn2");
+event.onclick = function() {
+  window.location.href = "events.html";
+}
+const event2 = document.getElementById("btn3");
+event2.onclick = function() {
+  window.location.href = "more.html";
+}
